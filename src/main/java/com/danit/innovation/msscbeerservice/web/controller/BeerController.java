5cabf3ebf1d58828553cf432ccc326d21429499c
rusud <d.rusu@house-of-communication.com>
2024-02-14 20:43:14 +0200

    adding beer controller

---
+++ src/main/java/com/danit/innovation/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,31 @@
+package com.danit.innovation.msscbeerservice.web.controller;
+
+import com.danit.innovation.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Created by rusud on 14.02.2024
+ */
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
