0c664797231f65a114c2287b04d54fd1977667cd
rusud <d.rusu@house-of-communication.com>
2024-02-14 20:34:58 +0200

    added web data model

---
+++ src/main/java/com/danit/innovation/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,19 @@
+package com.danit.innovation.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created by rusud on 14.02.2024
+ */
+public class BeerPagedList extends PageImpl<BeerDto> {
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/com/danit/innovation/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package com.danit.innovation.msscbeerservice.web.model;
+
+/**
+ * Created by rusud on 14.02.2024
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, ALE, STOUT, GOSE, IPA
+}
